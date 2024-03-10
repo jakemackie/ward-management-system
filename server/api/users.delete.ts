d199aa3373654fdd245bd9a567b2f6175308e086
Jake Mackie <jake.wmackie@gmail.com>
2024-03-10 21:38:00 +0000

    Delete

--- server/api/users.delete.ts
+++ server/api/users.delete.ts
@@ -1,5 +1,17 @@
-export default defineEventHandler((event) => {
-  return {
-    api: "delete users",
-  };
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  let user = null;
+
+  if (body.id)
+    user = await prisma.user.delete({
+      where: {
+        id: body.id,
+      },
+    });
+
+  return user;
 });
