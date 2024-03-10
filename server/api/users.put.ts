d76b9fc45d337ccfe4ea8ee9f5f1d571edc8e457
Jake Mackie <jake.wmackie@gmail.com>
2024-03-10 21:51:31 +0000

    Users api

--- server/api/users.put.ts
+++ server/api/users.put.ts
@@ -1,5 +1,29 @@
-export default defineEventHandler((event) => {
-  return {
-    api: "update users",
-  };
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const id = body.id;
+  const name = body.name;
+
+  if (!(id && name))
+    return createError({
+      statusCode: 400,
+      statusMessage: "Missing id and name",
+    });
+
+  let user;
+
+  if (id && name)
+    user = await prisma.user.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        name: body.name,
+      },
+    });
+
+  return user;
 });
