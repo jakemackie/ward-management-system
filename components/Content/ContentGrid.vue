e794887f9608ba447cfb47a2154252c92a8245b7
Jake Mackie <jake.wmackie@gmail.com>
2024-03-13 00:12:57 +0000

    Fixed build corruption

--- components/Content/ContentGrid.vue
+++ components/Content/ContentGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid w-full grid-cols-1 gap-4 *:rounded-lg *:border *:p-2 md:grid-cols-2"
+    class="grid w-full grid-cols-1 gap-4 *:rounded-lg *:border *:p-2 lg:grid-cols-2"
   >
     <slot></slot>
   </div>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="hidden md:block">
-    <nav class="min-h-screen w-24 border-r lg:w-72">
-      <ul class="flex flex-col justify-center space-y-2 py-10">
-        <NuxtLink to="/">
-          <SidebarItem :active="isActive('/')">
-            <SidebarButton>
-              <Icon name="ph:house-fill" />
-            </SidebarButton>
-            <SidebarLabel>Dashboard</SidebarLabel>
-          </SidebarItem>
-        </NuxtLink>
+  <nav class="min-h-screen w-24 border-r lg:w-72">
+    <ul class="flex flex-col justify-center space-y-2 py-10">
+      <NuxtLink to="/">
+        <SidebarItem :active="isActive('/')">
+          <SidebarButton>
+            <Icon name="ph:house-fill" />
+          </SidebarButton>
+          <SidebarLabel>Dashboard</SidebarLabel>
+        </SidebarItem>
+      </NuxtLink>
 
-        <NuxtLink to="/beds">
-          <SidebarItem :active="isActive('/beds')">
-            <SidebarButton>
-              <Icon name="ph:bed-fill" />
-            </SidebarButton>
-            <SidebarLabel>Beds</SidebarLabel>
-          </SidebarItem>
-        </NuxtLink>
-      </ul>
-    </nav>
-  </div>
+      <NuxtLink to="/beds">
+        <SidebarItem :active="isActive('/beds')">
+          <SidebarButton>
+            <Icon name="ph:bed-fill" />
+          </SidebarButton>
+          <SidebarLabel>Beds</SidebarLabel>
+        </SidebarItem>
+      </NuxtLink>
+    </ul>
+  </nav>
 </template>
 
 <script>
