a1503435ced225b30ce97a5dacd314ffdc8131fe
Jake Mackie <jake.wmackie@gmail.com>
2024-03-15 21:14:50 +0000

    Removed a single line because it annoyed me

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,6 @@
         <ul class="h-full flex justify-center items-center space-x-4">
             <MenuItem icon="material-symbols:bed" href="/">View Beds</MenuItem>
             <MenuItem icon="material-symbols:user-attributes" href="/beds">View Patient Details</MenuItem>
-
         </ul>
     </div>
 </template>
