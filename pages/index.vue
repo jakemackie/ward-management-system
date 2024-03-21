d29635fbd0e92805c17300934e5df961c45f2966
Jake Mackie <jake.wmackie@gmail.com>
2024-03-21 12:34:36 +0000

    Fixed grid scaling

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,22 @@
     <div>
         <h2>Manage Beds</h2>
         <p>This is the landing page where you can see an overview of the occupied beds per ward. From here&comma; you can click on a bed for its details.</p>
-        <div class="mt-10 grid h-32 grid-cols-2 md:grid-cols-3 gap-4">
-            <div class="size-full border border-red-500">a</div>
-            <div class="size-full border border-red-500">b</div>
-            <div class="size-full border border-red-500">c</div>
-            <div class="size-full border border-red-500">d</div>
+        <div class="mt-10 grid grid-cols-1 xl:grid-cols-2 gap-4">
+            <div class="flex">
+                <div class="flex-grow h-36 md:h-56 p-2 bg-slate-200">
+                    Example Bed 1
+                </div>
+            </div>
+            <div class="flex">
+                <div class="flex-grow h-36 md:h-56 p-2 bg-slate-200">
+                    Example Bed 2
+                </div>
+            </div>
+            <div class="flex">
+                <div class="flex-grow h-36 md:h-56 p-2 bg-slate-200">
+                    Example Bed 3
+                </div>
+            </div>
         </div>
     </div>
 </template>
